bbc689b274dad7d454677b2b342a4fc8ef6552a3
kvishalthakur <vishal.thakur@accionlabs.com>
2019-02-13 10:37:48 +0530

    basic project setup commited

---
+++ TransactionDataUpdate/src/main/java/com/tdu/TransactionDataUpdate/TransactionDataUpdateApplication.java
@@ -0,0 +1,14 @@
+package com.tdu.TransactionDataUpdate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionDataUpdateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionDataUpdateApplication.class, args);
+	}
+
+}
+
---
+++ TransactionDataUpdate/src/test/java/com/tdu/TransactionDataUpdate/TransactionDataUpdateApplicationTests.java
@@ -0,0 +1,17 @@
+package com.tdu.TransactionDataUpdate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TransactionDataUpdateApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
